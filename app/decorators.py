8b5993f14aea95b1088aa626fc6fab78822ffbe1
LeeWoonHee <zero1158@naver.com>
2025-07-22 17:30:59 +0900

    ep10 업데이트

---
+++ app/decorators.py
@@ -0,0 +1,24 @@
+#udemy ep10
+
+
+def custom_fence(fence: str = "+"):
+    def fence_decorator(func):
+        def wrapper(text : str):
+            print(fence *len(text))
+            func(text)
+            print(fence *len(text))
+        return wrapper
+    return fence_decorator
+
+
+    
+    
+
+
+
+
+@custom_fence('ㅗ')
+def log(text : str):
+    print(text)
+
+log('fuck')
--- main.py
+++ main.py
@@ -9,6 +9,6 @@ async def get_shipment():
     return {"content": "wooden table", "status": "in transit"}
 
 
-@app.get("/scalar", include_in_schema=False)
+@app.get("/scalar", include_in_schema=False) 
 async def get_scalar_docs():
     return get_scalar_api_reference(openapi_url=app.openapi_url, title="scalar api")
