f65e8b0acfb689f26319a8ffec2143e015c2882d
LeeWoonHee <zero1158@naver.com>
2025-07-22 16:30:42 +0900

    ep 9 다시 업데이트

--- app/type_hints.py
+++ app/type_hints.py
@@ -1,4 +1,11 @@
+from typing import Any
+
 # udemy ep9
+
+class City : 
+    def __init__(self,name,location):
+        self.name = name
+        self.location = location
 
 text: str = "value"
 pert: int = 90
@@ -8,12 +15,22 @@ number: int | float = 12
 
 digits : list[int] = [1,2,3,4,5]
 
-table_5 : tuple[int,int,int,int,int] = (5,10,15,20,25)
+table_5 : tuple[int,...] = (5,10,15,20,25)
+hampshire = City("hamspshire", 202424)
+city_temp : tuple[City,float] = (hampshire,20.5)
 
-city_temp : tuple[str,float] = ('city' : 20.5)
+
+shipment : dict[str,Any] = {
+    "id": 123,
+    "weight" : 1.2,
+    "content": "wooden table",
+    "status": "in transit"
+}
+
+
 
 def root(num: int | float, exp: float | None = .5) -> float:
     return pow(num, exp)
 
-
 root_25 = root(25)
+
